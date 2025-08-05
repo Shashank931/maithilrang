a9121902dc1b6c4fc47d9c5f85564d88cc7029cb
Shashank931 <sahiljha9192@gmail.com>
2025-08-05 10:23:11 +0530

    many changes and add some more file for features

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,28 @@
 from django.urls import path
-from core.views import home , login , signup , aboutus , paintings ,contactus , product_details
+from core.views import home , login , signup , aboutus , paintings ,contactus , product_details , cart , add_cart , remove_cart , remove_cart_item
 
 urlpatterns = [
+
     path('',home, name = 'home' ),
+
     path('login/',login, name = 'login' ),
+
     path('signup/',signup, name = 'signup' ),
+
     path('aboutus/',aboutus, name = 'aboutus' ),
+
     path('contactus/',contactus, name = 'contactus' ),
+
     path('paintings/',paintings, name = 'paintings' ),
+
     path('paintings/<slug:category_slug>/',paintings, name = 'paintings_by_category' ),
+
     path('paintings/<slug:category_slug>/<slug:product_slug>/',product_details, name = 'product_details' ),
+
+    path('cart/', cart , name='cart'),
+    path('add_cart/<int:product_id>/' , add_cart , name = 'add_cart'),
+    path('remove_cart/<int:product_id>/' , remove_cart , name = 'remove_cart'),
+    path('remove_cart_item/<int:product_id>/' , remove_cart_item , name = 'remove_cart_item'),
+
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404 , redirect
 from store.models import Product
 from category.models import Category
+from carts.models import Cart , CartItem 
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your views here.
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
 
 def home(req):
     products= Product.objects.all().filter(is_available = True)
@@ -38,14 +49,93 @@ def paintings(req , category_slug=None):
 def product_details(req , category_slug , product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug , slug = product_slug )
+
+        # in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(req) , product = single_product).exists()
     except Exception as e:
         raise e
     
     context ={
         'single_product' : single_product,
+        # 'in_cart' : in_cart
     }
 
     return render(req , 'core/product_details.html' , context)
+
+
+
+
+
+def add_cart(request , product_id ):
+    product = Product.objects.get(id=product_id) #get the product
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart _id present in the session
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product , cart = cart)
+        cart_item.quantity += 1 #cart_item.quantity = cart+item.quantity+1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product, 
+            quantity =1 ,
+            cart = cart,
+        )
+        cart_item.save()
+    return redirect('cart')
+
+def remove_cart(req , product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(req))
+    product = get_object_or_404(Product , id=product_id)
+    cart_item = CartItem.objects.get(product = product , cart=cart)
+    if cart_item.quantity>1:
+        cart_item.quantity -=1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def remove_cart_item (req , product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(req))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product , cart= cart)
+    cart_item.delete()
+    return redirect ('cart')
+
+
+
+
+
+
+
+def cart(req, total=0, quantity=0):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(req))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for item in cart_items:
+            total += (item.product.price * item.quantity)
+            quantity += item.quantity
+        tax = (2 * total)/100
+        grand_total = total+tax
+    except ObjectDoesNotExist:
+        cart_items = []
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,  
+        'tax' : tax,
+        'grand_total' : grand_total,
+
+    }
+
+    return render(req, 'core/cart.html', context)
+
 
 
 
